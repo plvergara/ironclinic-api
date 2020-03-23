74bf792f4a6ba0d77ab5ba7e2fff44783e717882
Pedro Luis Vergara <gudurix@MacBook-Pro-de-Pedro.local>
2020-03-23 19:22:28 +0100

    return commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://heroku_qr7t0t2q:s6hg1i9tasae0ckni8l7v2lmt2@ds127736.mlab.com:27736/heroku_qr7t0t2q'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ironclinic-api';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
