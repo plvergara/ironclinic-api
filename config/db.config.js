04c5013b3ec115525d9d3cf4056431576c3264b0
Pedro Luis Vergara <gudurix@MacBook-Pro-de-Pedro.local>
2020-03-23 18:28:14 +0100

    changed db.config

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = env.MONGODB_URI
+const MONGODB_URI = 'mongodb://heroku_qr7t0t2q:s6hg1i9tasae0ckni8l7v2lmt2@ds127736.mlab.com:27736/heroku_qr7t0t2q'
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
